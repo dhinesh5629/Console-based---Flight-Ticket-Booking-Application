ea107ecee2eafb27deb9baa4e21b3ca9906cc1e0
Dhinesh D <inithan02@gmail.com>
2024-02-04 21:36:36 +0530

    Update TicketBooker.java

--- TicketBooker.java
+++ TicketBooker.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class TicketBooker {
+public class TicketBooker {                          // Main class
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
